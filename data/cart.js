2d4a30e2dfac53e1d72f3a128b60cbd4948f59e1
Janasu97 <62239594+Janasu97@users.noreply.github.com>
2024-10-13 18:18:22 +0200

    Adding jasmine tests for cartTest

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,14 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = []
+loadFromStorage()
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = []
+    }
+    
 }
 
 function saveToStorage() {
